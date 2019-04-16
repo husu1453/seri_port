a41767c8aaf6d3b441c75b6235e8c36740a61887
husu1453 <42200312+husu1453@users.noreply.github.com>
2019-04-16 21:30:36 +0300

    Delete seri_port_kullanimi.ino

--- seri_port_kullanimi.ino
+++
@@ -1,7 +0,0 @@
-void setup() { 
-Serial.begin(9600);//Su an Arduino ve bilgisayar arsaında bir haberleşme başlattık.
-}//Void setup kisminda ledimizin pini gibi şeyler belirtiriz.Bu kısmdaki kodlar sadece bir kere okunur tekrarlanmaz.
-void loop() {
-Serial.println("Arduino Kafası");//Serial.println yazdık çünkü böyle daha okunabilir bir halde oluyor.Siz isterseniz (Serial.print) yazabilsiniz.
-
-}//Void loop kIsmI her seferinde tekrarlanır.Loop = Dongu
